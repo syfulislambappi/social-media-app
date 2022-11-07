9166c5f93332a6345a3948feaf3452170e8d2dbe
Syful Islam <48632407+syfulislambappi@users.noreply.github.com>
2022-11-07 23:54:09 +0600

    Adding recommended post feature

--- back-end/app/error.js
+++ back-end/app/error.js
@@ -11,6 +11,6 @@ exports.applicationErrorHandler = (err, _req, res, _next) => {
   if (!!err.status) {
     return res.status(err.status).json({ message: err.message });
   } else {
-    res.status(500).json({ message: "Internal server error." });
+    res.status(500).json({ message: err.message });
   }
 };
